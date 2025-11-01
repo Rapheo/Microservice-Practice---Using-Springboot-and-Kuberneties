35c400b0a7f7b3455a65d85b2352f04e3f36663e
Rapheo <abdullah.rapheo@g.bracu.ac.bd>
2025-11-01 10:21:29 +0600

    Structure created for productService

---
+++ ProductService/src/main/java/com/MicroservicePractice/ProductService/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.MicroservicePractice.ProductService.service;
+
+public interface ProductService {
+}
---
+++ ProductService/src/main/java/com/MicroservicePractice/ProductService/service/serviceImpl/ProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.MicroservicePractice.ProductService.service.serviceImpl;
+
+import com.MicroservicePractice.ProductService.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+}
